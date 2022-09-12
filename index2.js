e746699e786ac1d29c39e366136e8162e50287dd
dej-xx01 <101282484+dej-xx01@users.noreply.github.com>
2022-09-12 05:30:53 +0100

    added new files

---
+++ index2.js
@@ -0,0 +1,5 @@
+let ousEl = document.getElementById("ous-el");
+let p = "Not available in your region."
+function purchase(){
+    ousEl.textContent = p; // or ousEl.textcontent = "Not available in your region."
+}
